5690dcccb715266e547dc3bbbab1bd3d1580818c
Jasid72 <Jasidhassan@gmail.com>
2024-10-07 01:45:58 +0500

    Only Onwer Added also limit add

--- contracts/multiSig_Wallet.sol
+++ contracts/multiSig_Wallet.sol
@@ -2,6 +2,35 @@
 pragma solidity ^0.7.5;
 
 contract MultiSig{
+
+    address[] public owners;
+    uint limit;
+    constructor (address[] memory _owners, uint _limit) {
+        owners = _owners;
+        limit = _limit;
+    }
+
+    modifier onlyOnwers() {
+        bool owner = false;
+        for(uint i=0; i<=owners.length; i++){
+            if(owners[i] == msg.sender){
+                owner = true;
+            }
+        }
+        require(owner == true);
+        _;
+    }
+
+    struct Transfer{
+        uint amount;
+        address payable receiver;
+        uint approvals;
+        bool hasBeensend;
+        uint id;
+    }
+
+    Transfer[] transferRequest;
+
     mapping(address => uint) balance;
     event balancedAdded(uint amount, address depositeTo);
     function Deposit() payable public returns (uint){
@@ -10,14 +39,8 @@ contract MultiSig{
         return balance[msg.sender];
     }
 
-    address owner;
-    constructor () {
-        owner = msg.sender;
+    function createTransfer(uint _amount, address payable _receiver) public onlyOnwers{
+        transferRequest.push(Transfer(_amount, _receiver, 0, false, transferRequest.length));
     }
-
-    function getOwner() public view  returns (address)
-    {
-        return owner;
-    }
-
+    //
 }
